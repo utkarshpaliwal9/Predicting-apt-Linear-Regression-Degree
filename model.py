88d315564a395ae9cbdf4ea5445681565ee0b387
Utkarsh Paliwal <utkarshpaliwal@users.noreply.github.com>
2018-01-05 00:05:43 +0530

    Positions - model2
    The model2.py file works on Position_Salaries.csv and works out the apt degree on the real dataset. Book1.csv is a fake dataset build to test if the model works correctly

---
+++ model.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 22:44:09 2018
+
+@author: Utkarsh
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Book1.csv')
+X = dataset.iloc[:, 0:1].values
+y = dataset.iloc[:, 1].values
+
+'''
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+from sklearn.linear_model import LinearRegression
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)'''
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+deg=1; min = float('Inf'); apt_deg = 1
+while deg<=10:
+    poly_reg = PolynomialFeatures(degree = deg)
+    X_poly_temp = poly_reg.fit_transform(X)
+    poly_reg.fit(X_poly_temp, y)
+    lin_reg_2 = LinearRegression()
+    lin_reg_2.fit(X_poly_temp, y)
+    y_pred = lin_reg_2.predict(X_poly_temp)
+    mse = 0
+    for i in range(10):
+        mse = mse + ((y_pred[i]-y[i])**2)
+    mse/=20
+    if mse < min:
+        min = mse
+        apt_deg = deg
+    deg+=1
+
+poly_reg = PolynomialFeatures(degree = apt_deg)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Best Regressor (Polynomial Regression)')
+plt.xlabel('X axis label')
+plt.ylabel('Y axis label')
+plt.show()
